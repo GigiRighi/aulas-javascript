d269b325749a4490d59dcfec145f48a37823723e
Giovana Righi <giovana.righi@aluno.senai.br>
2026-02-06 14:42:43 -0300

    testando preço refri

--- atividade/atividade.js
+++ atividade/atividade.js
@@ -5,5 +5,9 @@ const duracao = parseFloat(prompt('Quantas horas a festa vai durar?'))
  const salgadinhosGeral = (qtdPessoas * 18) / 100 //18 é a quantidade de salgadinho // 100 é o cento
  const precoSalgadinho = salgadinhosGeral * 48.90 //48.90 é o preço do cento
 
- alert (`o preço salgadinho ${precoSalgadinho}`)
+const docinhosGeral = (qtdPessoas * 6) / 20 //6 é a quantidade de salgadinho por pessoa // 20 é a unidade vendida
+const precoDocinhos = docinhosGeral * 42.90 // 42.90 é o preço por 20 unidades
 
+const refriGeral = qtdPessoas / 2 // 
+const precoRefri = refriGeral * 8.99
+alert (`preço refri ${precoRefri} `)

4a2793c079bded3e7c0b2b49dbdd2354c153cb16
Giovana Righi <giovana.righi@aluno.senai.br>
2026-02-06 15:41:53 -0300

    finalizando

--- atividade/atividade.js
+++ atividade/atividade.js
@@ -8,6 +8,34 @@ const duracao = parseFloat(prompt('Quantas horas a festa vai durar?'))
 const docinhosGeral = (qtdPessoas * 6) / 20 //6 é a quantidade de salgadinho por pessoa // 20 é a unidade vendida
 const precoDocinhos = docinhosGeral * 42.90 // 42.90 é o preço por 20 unidades
 
-const refriGeral = qtdPessoas / 2 // 
+const refriGeral = qtdPessoas / 2 // divide por dois pois é o litro do refri 
 const precoRefri = refriGeral * 8.99
-alert (`preço refri ${precoRefri} `)
+
+const copos = (qtdPessoas * 5) / 100
+const precoCopos = copos * 12.99
+
+const pratos = (qtdPessoas * 2) / 10
+const precoPrato = pratos * 4.99
+
+const garfoFaca = (qtdPessoas * 2) / 10
+const precoGarfoFaca = garfoFaca * 7.99
+
+const mesaCadeira = qtdPessoas / 4
+const precoMesaCadeira = qtdPessoas * 17.99
+
+const karaoke = duracao / 2
+const precoKaraoke = karaoke * 27.99
+
+const aluguelEspaco = duracao * 239.99
+
+//divisão dos itens comunitários
+
+const valorPessoa = (precoMesaCadeira + karaoke + aluguelEspaco) / pessoasAdultas
+
+alert(`O valor que cada um pagará por pessoa é R$${valorPessoa} `)
+
+// o que você terá que pagar 
+
+const valorAnfitriao = (precoSalgadinho + precoDocinhos + precoRefri + precoCopos + precoPrato + precoGarfoFaca)
+alert(`Esse será o valor que o você irá pagar R$${valorAnfitriao}`)
+

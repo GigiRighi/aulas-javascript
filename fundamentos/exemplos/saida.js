9f6a0c03ac32f5215ce9853cf26ff79c93177747
Giovana Righi <giovana.righi@aluno.senai.br>
2026-02-06 10:54:40 -0300

    fazendo exercicio

---
+++ fundamentos/exemplos/saida.js
@@ -0,0 +1,10 @@
+const nome = prompt("Qual seu primeiro nome?")
+const endereco = prompt("Qual seu endereço?")
+const idade = prompt("qual a sua idade?")
+
+alert(`
+    ${nome}, você tem ${idade} anos, \n mora no endereço ${endereco}`)
+
+
+
+    
